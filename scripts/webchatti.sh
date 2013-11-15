4ba1eca9db1fee507ff7c7d0d5557bb3b38076cd
Anto Binish Kaspar <anto@roanuz.com>
2013-11-16 00:28:59 +0530

    Added download script

--- scripts/webchatti.sh
+++ scripts/webchatti.sh
@@ -3,5 +3,18 @@
 BASE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 BASE="$(dirname "$BASE")"
 PWD=`pwd`
-URL="http://localhost:8000/"
+WORK=chattipackage
+URL="https://github.com/roanuz/webchatti"
+TAR="$URL/tarball/master"
 
+mkdir $WORK
+echo "Downloading $TAR"
+curl -L $TAR | tar zx -C $WORK --strip-components 1
+# tar -C $WORK --strip-components 1 -zxf roanuz-webchatti-9ecea1f.tar.gz
+mv $WORK/chatties/default/* .
+rm -rf $WORK
+python -m SimpleHTTPServer
+
+echo "========"
+echo "Thanks for using Web Chatti!"
+echo "========"
